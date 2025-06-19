326c56505c64aad0b2bb2dfa95d0526e6dfe76ef
Ayush6659 <ayush8584@gmail.com>
2025-06-19 13:57:07 +0530

    Added responsiveness to the web page

--- src/Components/Users/Register.jsx
+++ src/Components/Users/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CgNametag } from "react-icons/cg";
 import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
 import { MdAttachEmail } from "react-icons/md";
@@ -6,26 +6,45 @@ import { TbPassword } from "react-icons/tb";
 import { FaRepeat } from "react-icons/fa6";
 
 const Register = () => {
+let [formdata,setformdata] = useState({
+  username:"",
+  password:"",
+  email:"",
+  name:""
+})
+
+let handleChange=(e)=>{
+  let {name,value} =e.target
+  setformdata({...formdata,[name]:value})
+
+}
+let handleSubmit=(e)=>{
+  e.preventDefault()
+  console.log(formdata);
+  
+}
+
+
   return (
     <div className='bg-[#efef] w-full h-full flex items-center justify-center '>
-     <form action="" className='w-1/2  flex items-center justify-center flex-col gap-2 bg-white py-10 rounded-2xl px-10' >
+     <form action="" className='w-1/2  flex items-center justify-center flex-col gap-2 bg-white py-10 rounded-2xl px-10 max-sm:w-[90%] ' onSubmit={handleSubmit} >
       <div className='w-full  flex justify-center items-center h-10 '>
-        <h1 className='text-2xl font-bold'>Register Form</h1>
+        <h1 className='text-2xl font-bold max-sm:text-sm'>Register Form</h1>
       </div>
       <div className='w-full flex justify-center items-center h-10 border-[2px]  '>
-        <input type="text" name="" placeholder='Enter Name' className='w-full outline-0 text-md px-5 font-bold p'/>
+        <input type="text" name='name' placeholder='Enter Name' className='w-full outline-0 text-md px-5 font-bold p' onChange={handleChange}/>
         <span><CgNametag /></span>
       </div>
       <div className='w-full flex justify-center items-center h-10 '>
-        <input type="text" name="" placeholder='Enter Username' className='w-full outline-0 text-md px-5 font-bold p' />
+        <input type="text" name='username' placeholder='Enter Username' className='w-full outline-0 text-md px-5 font-bold p' onChange={handleChange}/>
         <span><MdOutlineDriveFileRenameOutline /></span>
       </div>
       <div className='w-full flex justify-center items-center h-10 '>
-        <input type="email" name="" placeholder='Enter Your email'  className='w-full outline-0 text-md px-5 font-bold p'/>
+        <input type="email" name="email" placeholder='Enter Your email'  className='w-full outline-0 text-md px-5 font-bold p'onChange={handleChange}/>
         <span><MdAttachEmail /></span>
       </div>
       <div className='w-full flex justify-center items-center h-10 '>
-        <input type="password" placeholder='Enter Password'  className='w-full outline-0 text-md px-5 font-bold p'/>
+        <input type="password" name='password' placeholder='Enter Password'  className='w-full outline-0 text-md px-5 font-bold p'onChange={handleChange}/>
         <span><TbPassword /></span>
       </div>
       <div className='w-full  flex justify-center items-center h-10 '>
