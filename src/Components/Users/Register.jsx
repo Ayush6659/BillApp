a97bd13a47a08bb8f708c049e9271bc42648e731
Ayush6659 <ayush8584@gmail.com>
2025-06-20 23:35:00 +0530

    validation of regestration form completed

--- src/Components/Users/Register.jsx
+++ src/Components/Users/Register.jsx
@@ -4,68 +4,99 @@ import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
 import { MdAttachEmail } from "react-icons/md";
 import { TbPassword } from "react-icons/tb";
 import { FaRepeat } from "react-icons/fa6";
-import {validatePassword} from "val-pass"
+import { validatePassword } from "val-pass"
+import {toast} from 'react-hot-toast'
 
 const Register = () => {
-const [formdata,setformdata] = useState({
-  username:"",
-  password:"",
-  email:"",
-  name:""
-})
+  const [formdata, setformdata] = useState({
+    username: "",
+    password: "",
+    email: "",
+    name: ""
+  })
 
-const[matched,setMatched]=useState(true)
-const {validateAll,getAllValidationErrorMessage}=validatePassword("password",8)
+  const [matched, setMatched] = useState(true)
+  const [errorMessage, setErrorMessage] = useState("")
 
-const handleChange=(e)=>{
-  let {name,value} =e.target
-  setformdata({...formdata,[name]:value})
+  const handleChange = (e) => {
+    let { name, value } = e.target
+    if (name=="password") {
+     let {validateAll,getAllValidationErrorMessage } = validatePassword(value,8)
+    //  console.log(validateAll());
+     
+      validateAll()?setErrorMessage(""):setErrorMessage(getAllValidationErrorMessage())
+      value==""&&setErrorMessage("")
+    }
 
-}
-const handleSubmit=(e)=>{
-  e.preventDefault()
-  console.log(formdata);
-  
-  
-}
-const handleRepeatPass=(e)=>{
-  const {value}=e.target
-  formdata.password!=value?setMatched(false):setMatched(true)
-  value==""&&setMatched(true)
 
-}
+    setformdata({ ...formdata, [name]: value })
+
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    let {username,name,password,email}=formdata
+    if(!name||!username||!password||!email){
+      toast.error("check all fields")
+      return
+    }
+    let {validateAll,getAllValidationErrorMessage}=validatePassword(password)
+    if(!validateAll){
+      toast.error(`${getAllValidationErrorMessage()}`)
+      return
+    }
+    if(!matched){
+      toast.error("password did not match")
+      return
+
+    }
+    
+    
+    console.log(formdata);
+  }
+  const handleRepeatPass = (e) => {
+    const { value } = e.target
+    formdata.password != value ? setMatched(false) : setMatched(true)
+    value == "" && setMatched(true)
+
+  }
+
 
 
   return (
-    <div className='bg-[#efef] w-full h-full flex items-center justify-center '>
-     <form action="" className='w-1/2  flex items-center justify-center flex-col gap-2 bg-white py-10 rounded-2xl px-10 max-sm:w-[90%] ' onSubmit={handleSubmit} >
-      <div className='w-full  flex justify-center items-center h-10 '>
-        <h1 className='text-2xl font-bold max-sm:text-sm'>Register Form</h1>
-      </div>
-      <div className='w-full flex justify-center items-center h-10 border-[2px]  '>
-        <input type="text" name='name' placeholder='Enter Name' className='w-full outline-0 text-md px-5 font-bold p' onChange={handleChange}/>
-        <span><CgNametag /></span>
-      </div>
-      <div className='w-full flex justify-center items-center h-10 '>
-        <input type="text" name='username' placeholder='Enter Username' className='w-full outline-0 text-md px-5 font-bold p' onChange={handleChange}/>
-        <span><MdOutlineDriveFileRenameOutline /></span>
-      </div>
-      <div className='w-full flex justify-center items-center h-10 '>
-        <input type="email" name="email" placeholder='Enter Your email'  className='w-full outline-0 text-md px-5 font-bold p'onChange={handleChange}/>
-        <span><MdAttachEmail /></span>
-      </div>
-      <div className='w-full flex justify-center items-center h-10 '>
-        <input type="password" name='password' placeholder='Enter Password'  className='w-full outline-0 text-md px-5 font-bold p'onChange={handleChange}/>
-        <span><TbPassword /></span>
-      </div>
-      <div className={`w-full border-2 flex justify-center items-center h-10 ${!matched? `border-red-700`: `border-black`} `}>
-        <input type="password" placeholder='Re-type your password'  className='w-full outline-0 text-md px-5 font-bold p' onChange={handleRepeatPass}/>
-        <span><FaRepeat /></span>
-      </div>
-      <div className='w-full  flex justify-center items-center h-10 bg-black text-amber-50 '>
-        <button className='font-bold tracking-widest'>click</button>
-      </div>
-     </form>
+    <div className='bg-[#efef] w-full h-full flex items-center justify-center'>
+      <form action="" className='w-1/2 h-[80%] flex items-center flex-col gap-2 bg-white py-10 rounded-2xl px-10 max-sm:w-[90%] overflow-scroll  ' onSubmit={handleSubmit} >
+        <div className='w-full  flex justify-center items-center h-10 '>
+          <h1 className='text-2xl font-bold max-sm:text-sm'>Register Form</h1>
+        </div>
+        <div className='w-full flex justify-center items-center h-10 border-[2px] px-[10px]  '>
+          <input type="text" name='name' placeholder='Enter Name' className='w-full outline-0 text-md px-5 font-bold ' onChange={handleChange} />
+          <span><CgNametag /></span>
+        </div>
+        <div className='w-full flex justify-center items-center h-10 border-[2px] px-[10px]'>
+          <input type="text" name='username' placeholder='Enter Username' className='w-full outline-0 text-md px-5 font-bold ' onChange={handleChange} />
+          <span><MdOutlineDriveFileRenameOutline /></span>
+        </div>
+        <div className='w-full flex justify-center items-center h-10  border-[2px] px-[10px]'>
+          <input type="email" name="email" placeholder='Enter Your email' className='w-full outline-0 text-md px-5 font-bold ' onChange={handleChange} />
+          <span><MdAttachEmail /></span>
+        </div>
+        <div className='w-full flex justify-center items-center h-10  border-[2px] px-[10px]'>
+          <input type="password" name='password' placeholder='Enter Password' className='w-full outline-0 text-md px-5 font-bold ' onChange={handleChange} />
+          <span><TbPassword /></span>
+        </div>
+        <div className={errorMessage?'w-full flex justify-center items-center px-3 rounded-sm':'hidden'}>
+          <span className='text-red-700'>*{errorMessage}</span>
+        </div>
+        <div className={`w-full  flex justify-center items-center  border-[2px] px-[10px] h-10 ${!matched ? `border-red-700` : `border-black`} `}>
+          <input type="password" placeholder='Re-type your password' className='w-full outline-0 text-md px-5 font-bold  ' onChange={handleRepeatPass} />
+          <span><FaRepeat /></span>
+        </div>
+        <div className='w-full  flex justify-center items-center h-10 bg-black text-amber-50 '>
+          <button className='font-bold tracking-widest w-full h-full hover:bg-blue-300'>click</button>
+        </div>
+
+      </form>
     </div>
   )
 }
